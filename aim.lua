6e4d7a4d3a07e8842b2e21572b977ac3397dcc97
laitung1122 <137845259+laitung1122@users.noreply.github.com>
2024-09-01 23:10:32 +0700

    Update aim.lua

--- aim.lua
+++ aim.lua
@@ -31,7 +31,7 @@ Main.Name = "Main"
 Main.Parent = infoplayers
 Main.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 Main.BackgroundTransparency = 0.4  -- Adjust transparency
-Main.Position = UDim2.new(0.5, -Main.Size.X.Offset / 2, 0, 0)
+Main.Position = UDim2.new(0, 60, 0, 20)
 Main.Size = UDim2.new(0, 260, 0, 80)
 
 MainCorner.Name = "MainCorner"
