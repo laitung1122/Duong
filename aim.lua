eb564b72aaea3409cbd028999f95fa9a032f51e8
laitung1122 <137845259+laitung1122@users.noreply.github.com>
2024-09-01 20:12:17 +0700

    Update aim.lua

--- aim.lua
+++ aim.lua
@@ -24,7 +24,7 @@ Main.Name = "Main"
 Main.Parent = infoplayers
 Main.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 Main.BackgroundTransparency = 0.7
-Main.Position = UDim2.new(0, 40, 0, 10)  -- Di chuyển menu sang phải thêm một chút
+Main.Position = UDim2.new(0, 70, 0, 10)  -- Di chuyển menu sang phải thêm một chút
 Main.Size = UDim2.new(0, 263, 0, 100)  -- Chỉnh sửa kích thước để tạo hình chữ nhật
 
 local MainCorner = Instance.new("UICorner")
